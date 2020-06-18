707d1e76b07e80feba6491d2e004cf77fb022db7
Kusuma Nimushakavi <kusumanimushakavi@gmail.com>
2020-06-18 16:58:03 -0400

    Initial Commit

---
+++ MovieSuggester/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MovieSuggester
+//
+//  Created by Kusuma Nimushakavi on 6/18/20.
+//  Copyright © 2020 Kusuma Nimushakavi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
